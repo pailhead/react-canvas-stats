833c26d8df427c4decfbd5a563cf2ecada147400
Dusan Bosnjak <citizens.arrest@gmail.com>
2018-04-21 19:38:40 -0700

    build without react?
    not sure how this is supposed to work

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,41 @@
 const path = require('path')
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                exclude: /(node_modules|build)/,
-                use: {
-                    loader: 'babel-loader'
+const config = (env, argv) => {
+    console.log('env is ', env)
+
+    const config = {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'index.js',
+            libraryTarget: 'commonjs2'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    include: path.resolve(__dirname, 'src'),
+                    exclude: /(node_modules|build)/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
                 }
-            }
-        ]
+            ]
+        }
     }
+
+    if (argv.mode === 'production') {
+        config.externals = {
+            react: {
+                root: 'React',
+                'commonjs-module': 'react',
+                commonjs2: 'react',
+                commonjs: 'react',
+                amd: 'react'
+            }
+        }
+    }
+    return config
 }
+
+module.exports = config
